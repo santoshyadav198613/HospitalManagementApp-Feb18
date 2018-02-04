c15e096b8c0efa2fa39db0dd15640a3ac243727e
santoshyadav198613 <santosh.yadav198613@gmail.com>
2018-02-04 14:58:03 +0530

    added built-in directives

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Employee } from './employee';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
   title: string = 'Employee';
-  visible : boolean = false;
+  visible: boolean = false;
+  role: string = 'User';
+  employee: Employee[] = [
+    { id: 1, name: 'Test1', email: 'abc@gmail.com', salary: 12000 },
+    { id: 2, name: 'Test2', email: 'abc@gmail.com', salary: 24000 },
+    { id: 3, name: 'Test3', email: 'abc@gmail.com', salary: 48000 },
+    { id: 4, name: 'Test4', email: 'abc@gmail.com', salary: 120000 }
+  ]
   constructor() { }
 
   ngOnInit() {

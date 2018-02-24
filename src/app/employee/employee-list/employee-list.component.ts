781f9cf902749333c15236bfa68dad02c19e40a0
santoshyadav198613 <santosh.yadav198613@gmail.com>
2018-02-24 18:12:51 +0530

    added destroy event

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component, OnInit,
+  Input, Output, EventEmitter, OnChanges, SimpleChanges,
+  OnDestroy
+} from '@angular/core';
 import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
-  templateUrl: './employee-list.component.html',  
+  templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent implements OnInit, OnChanges {
+export class EmployeeListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() employeeList: Employee[];
   @Input() title: string;
   @Output() selectedEmployee: EventEmitter<Employee> = new EventEmitter<Employee>();
@@ -24,4 +28,7 @@ export class EmployeeListComponent implements OnInit, OnChanges {
     this.selectedEmployee.emit(emp);
   }
 
+  ngOnDestroy(): void {
+    alert('Do you really want to destroy emp list');
+  }
 }

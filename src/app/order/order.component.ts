4bd898d292e337a880dbe2de9b91533d4903dbf0
santoshyadav198613 <santosh.yadav198613@gmail.com>
2018-03-31 13:53:00 +0530

    added reactive forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
@@ -17,16 +17,43 @@ export class OrderComponent implements OnInit {
       {
         orderId: new FormControl('', [Validators.required, Validators.min(1)]),
         customerName: new FormControl('', [Validators.required, Validators.minLength(4)]),
-        email: new FormControl(''),
-        mobile: new FormControl(''),
+        email: new FormControl('',[Validators.required,Validators.email]),
+        mobile: new FormControl('',[Validators.required]),
         address: this.fb.group(
           {
-            addressLine1: new FormControl(''),
-            addressLine2: new FormControl(''),
-            city: new FormControl(''),
-            pin: new FormControl('')
-          })
+            addressLine1: new FormControl('',[Validators.required]),
+            addressLine2: new FormControl('',[Validators.required]),
+            city: new FormControl('',[Validators.required]),
+            pin: new FormControl('',[Validators.required])
+          }),
+        cards: this.fb.array([
+          this.buildForm()
+        ])
       });
   }
 
+  buildForm() {
+    return this.fb.group(
+      {
+        cardNumber: new FormControl('', [Validators.required]),
+        expMonth: new FormControl('', [Validators.min(1), Validators.max(12)]),
+        expYear: new FormControl('', [Validators.required,Validators.min(1), Validators.max(12)]),
+        cvv: new FormControl('', [Validators.required])
+      }
+    )
+  }
+
+  addCard() {
+    const card = this.orderForm.controls['cards'] as FormArray;
+    card.push(this.buildForm());
+  }
+
+  removeCard(i: number) {
+    const card = this.orderForm.controls['cards'] as FormArray;
+    card.removeAt(i);
+  }
+
+  saveOrder() {
+     console.log(this.orderForm.value);
+  } 
 }
